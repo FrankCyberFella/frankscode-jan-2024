b8228604dce747014db95141e22236965954ad55
Frank Fella <frank.fella@simplilearn.net>
2024-03-28 13:35:35 -0700

    Instructor Push 03/28/2024 13:35:35

---
+++ Phase-2-Backend-Java-and-Database-Development/01-Intro_To_Classes-Day-3/src/main/java/com/frank/Application_Program.java
@@ -0,0 +1,17 @@
+package com.frank;
+
+/**
+ * Application Program to Demonstrate Basic Use of Classes and Object
+ *
+ */
+public class Application_Program 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Welcome to our class and object usage demo!" );
+        
+        
+        
+        System.out.println("\nThanks for visiting our class and object usage demo!" );
+    }
+}
