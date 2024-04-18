45c49c1260c097d386b767099b934f785e8be771
Frank Fella <frank.fella@simplilearn.net>
2024-04-18 18:07:01 +0000

    Instructor Push 04/18/2024 18:07:01

---
+++ Phase-2-Backend-Java-and-Database-Development/02-Day-3-JDBC-Sample-Program-Done-In-Class-Frank/src/main/java/com/frank/exceptions/DataBaseDeleteException.java
@@ -0,0 +1,16 @@
+package com.frank.exceptions;
+
+public class DataBaseDeleteException extends Exception {
+
+	public DataBaseDeleteException() {
+		super();
+	}
+	
+	public DataBaseDeleteException(String message) {
+		super(message);
+		
+		System.out.println("Problem deleting data in database:");
+		System.out.println(message);
+	}
+}
+
